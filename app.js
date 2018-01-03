96dce60a2cab80b86222df9ec076a7f130dfc6d4
RyanSharafuddin <rzs_0000@hotmail.com>
2018-01-02 20:22:08 -0500

    Some bug where client connects repeatedly and server ignores other events

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-// var express = require('express');
-// var app = express();
-// app.use(express.static('public'));
-// var http = require('http').Server(app);
-// var port = 3000;
-//
-// http.listen(port, function() {
-//     console.log('listening on *: ' + port);
-// });
-console.log("Running app.js");
 var express = require('express');
 var app = express();
-var portNum = 8000;
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 8000;
 
-// app.get('/', function(req, res) {
-//   res.send("This is the root page");
-// });
+app.use(express.static('public'));
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+  console.log("Got request for homepage");
+});
 
-app.use(express.static('static_files'));
+io.on('connection', function(socket){
+   console.log('A client has connected to the server');
+   socket.on('move', function(moveString){
+     console.log("Move made: " + moveString);
+  //   io.emit('chat message', msg);
+   });
+  socket.on('disconnect', function(){
+    console.log('A client has disconnected from the server');
+  });
+});
 
-app.listen(portNum);
+http.listen(port, function() {
+    console.log('listening on *: ' + port);
+});
